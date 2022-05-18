04a834f2bfecc54a1fed41bbe199b4f1638ece77
jpb <jakub.baranski.33@gmail.com>
2022-05-18 18:43:00 +0100

    Add logic to ExtendedAppCompatActivity

--- src/main/java/com/jpb/appcompat/ExtendedAppCompatActivity.kt
+++ src/main/java/com/jpb/appcompat/ExtendedAppCompatActivity.kt
@@ -1,4 +1,23 @@
 package com.jpb.appcompat
 
-class ExtendedAppCompatActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ExtendedAppCompatActivity : AppCompatActivity() {
+
+    private val localeDelegate by lazy {
+        LocaleDelegate()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        localeDelegate.onCreate(this)
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (localeDelegate.isLocaleChanged) {
+            recreate()
+        }
+    }
 }
