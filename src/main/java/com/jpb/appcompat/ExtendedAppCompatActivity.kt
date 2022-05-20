a03a79ce8787bc99ee1df92923b5ecea0fc89ff8
jpb <jakub.baranski.33@gmail.com>
2022-05-20 18:20:11 +0100

    Fix bug

--- src/main/java/com/jpb/appcompat/ExtendedAppCompatActivity.kt
+++ src/main/java/com/jpb/appcompat/ExtendedAppCompatActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.jpb.appcompat.locale.LocaleDelegate
 
-class ExtendedAppCompatActivity : AppCompatActivity() {
+open class ExtendedAppCompatActivity : AppCompatActivity() {
 
     private val localeDelegate by lazy {
         LocaleDelegate()
